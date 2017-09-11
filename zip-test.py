7b2d7e0e92f8a7b29a2700003d9d6977e94aa33a
Pavan Chhatpar <pavanchhatpar@gmail.com>
2017-09-12 00:57:17 +0530

    fix: zip-test.py securely compresses using 7z and decompresses using zipfile

--- zip-test.py
+++ zip-test.py
@@ -12,10 +12,14 @@ input_file = open(input_file_path, "rb")
 
 #input_file_path = path + '/' + input_file_path
 #print (input_file_path)
-rc = subprocess.call(['7z', 'a', key, '-y', 'myzipfile.zip'] + [ '\a.txt' ])
+rc = subprocess.call(['7z', 'a', "-p"+key, '-y', 'myzipfile.zip'] + [ 'a.txt' ])
 
-zf = zipfile.ZipFile("myzipfile.zip", "w")
-zf.setpassword(k)
-zf.write(input_file_path)
+#zf = zipfile.ZipFile("myzipfile.zip", "w")
+#zf.setpassword(k)
+#zf.write(input_file_path)
 
-zf.extractall(pwd = k)
+#zf.extractall(path="extracted")
+print("Extracting contents now to folder 'extracted'")
+with zipfile.ZipFile("myzipfile.zip", "r") as zf:
+    zf.setpassword(k)
+    zf.extractall(path="extracted")
